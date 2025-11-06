74dd231d8875bf2f7c03d9b31a74904f7c2942cf
Muhammad Usama <Usama@Tekhqs.local>
2025-11-07 00:22:53 +0500

    Add Cors

--- backend/settings.py
+++ backend/settings.py
@@ -29,9 +29,22 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:5173",
     
     ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Application definition
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
